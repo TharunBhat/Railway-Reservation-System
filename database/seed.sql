536f7ad9db8fb0d6ecbc515292384d723f967e6e
TharunBhat <tharunbhatt@gmail.com>
2025-11-27 18:54:45 +0530

    Create seed.sql

---
+++ database/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO trains (train_name, number, source, destination, dep_time, arr_time, seats_total, seats_available)
+VALUES
+('Express One', 'EX001', 'A', 'B', '08:00', '12:00', 50, 50),
+('Express Two', 'EX002', 'A', 'C', '09:00', '13:30', 60, 60);
